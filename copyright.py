005f180253b607d589998ab7c1295f8122684121
darkmadara09 <136217534+darkmadara09@users.noreply.github.com>
2024-02-22 00:36:12 +0530

    Update copyright.py

--- copyright.py
+++ copyright.py
@@ -15,13 +15,13 @@ logging.basicConfig(level=logging.INFO)
 
 # -------------------------------------------------------------------------------------
 
-BOT_USERNAME = os.environ.get("BOT_USERNAME","Group_SecurityRobot")
+BOT_USERNAME = os.environ.get("BOT_USERNAME","ShadowsCopyrightbot")
 
-OWNER_ID = "6664582540"
+OWNER_ID = "6758236533"
 # -------------------------------------------------------------------------------------
 
-API_ID = "6435225" # 
-API_HASH = "4e984ea35f854762dcde906dce426c2d"
+API_ID = "21279292" # 
+API_HASH = "b9f12d911d85b4a60fa13ba9c2c09712"
 BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
 
 # --------------------------------------------------------------------------------------
@@ -35,11 +35,11 @@ app = Client('my_bot', api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 # -------------------------------------------------------------------------------------
 
 
-start_txt = """<b> 🤖 𝖦𝗋𝗈𝗎𝗉 𝖲𝖾𝖼𝗎𝗋𝗂𝗍𝗒 𝖱𝗈𝖻𝗈𝗍 🛡️ </b>
+start_txt = """<b> 🤖 ɢʀᴏᴜᴘ sᴇᴄᴜʀɪᴛʏ ʀᴏʙᴏᴛ 🛡️ </b>
 
-𝖶𝖾𝗅𝖼𝗈𝗆𝖾 𝗍𝗈 𝖦𝗋𝗈𝗎𝗉𝖲𝖾𝖈𝗎𝗋𝗂𝗍𝗒𝖱𝗈𝖻𝗈𝗍, 𝗒𝗈𝗎𝗋 𝗏𝗂𝗀𝗂𝗅𝖺𝗇𝗍 𝗀𝗎𝖺𝗋𝖾𝗂𝗇 𝗂𝗇 𝗍𝗁𝗂𝗌 𝖳𝖾𝗅𝖾𝗀𝗋𝖺𝗆 𝗌𝗉𝖺𝖼𝖾! 𝖮𝗎𝗋 𝗆𝗂𝗌𝗌𝗂𝗈𝗇 𝗂𝗌 𝗍𝗈 𝖾𝗇𝗌𝗎𝗋𝖾 𝖺 𝗌𝖾𝖼𝗎𝗋𝖾 𝖺𝗇𝖽 𝗉𝗅𝖾𝖺𝗌𝖺𝗇𝗍 𝖾𝗇𝗏𝗂𝗋𝗈𝗇𝗆𝖾𝗇𝗍 𝖿𝗈𝗋 𝖾𝗏𝖾𝗋𝗒𝗈𝗇𝖾. 𝖥𝗋𝗈𝗆 𝖼𝗈𝗉𝗒𝗋𝗂𝗀𝗁𝗍 𝗉𝗋𝗈𝗍𝖾𝖼𝗍𝂢𝗂𝗈𝗇 𝗍𝗈 𝗆𝖺𝗂𝗇𝗍𝖺𝗂𝗇𝗂𝗇𝗀 𝖽𝖾𝖼𝗈𝗋𝗎𝗆, 𝗐𝖾'𝗏𝖾 𝗀𝗈𝗍 𝗂𝗍 𝖼𝗈𝗏𝖾𝗋𝖾𝖽.
+ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ Sʜᴀᴅᴏᴡ Sᴇᴄᴜʀɪᴛʏ Rᴏʙᴏᴛ , ʏᴏᴜʀ sᴛᴜᴘɪᴅ ᴛʜɪɴɢs ᴡɪʟʟ ʙᴇ ʀᴇᴍᴏᴠᴇᴅ ʙʏ ᴛʜɪs ʙᴏᴛ ! ᴏᴜʀ ᴍɪssɪᴏɴ ɪs ᴛᴏ ᴇɴsᴜʀᴇ ᴀ sᴀғᴇ ᴀɴᴅ sᴇᴄᴜʀᴇ ɢʀᴏᴜᴘ ᴄʜᴀᴛ ᴀɴᴅ ᴍᴀɪɴᴛᴀɪɴ ᴘᴇᴀᴄᴇ !
 
-𝖥𝖾𝖾𝗅 𝖿𝗋𝖾𝖾 𝗍𝗈 𝗋𝖾𝗉𝗈𝗿𝗍 𝖺𝗇𝗒 𝖼𝗈𝗇𝖼𝖾𝗋𝗇𝗌, 𝖺𝗇𝖽 𝗅𝖾𝗍'𝗌 𝗐𝗈𝗋𝗄 𝗍𝗈𝗀𝖾𝗍𝗁𝖾𝗋 𝗍𝗈 𝗆𝖺𝗄𝖾 𝗍𝗁𝗂𝗌 𝖼𝗈𝗆𝗆𝗎𝗇𝗂𝗍𝗒 𝗍𝗁𝗋𝗂𝗏𝖾! 🤝🔐 """
+ɴᴏᴛᴇ - 𝟺 ᴄʜᴀᴀᴠᴀɴɪ ɢʜᴏᴅᴇ ᴘᴇ ʀɪᴠᴀʟs ʜᴜᴍᴀʀᴇ ʟᴏᴅᴇ ᴘᴇ"""
 
 @app.on_message(filters.command("start"))
 async def start(_, msg):
@@ -48,7 +48,7 @@ async def start(_, msg):
           InlineKeyboardButton("ᴀᴅᴅ ᴍᴇ", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
         ],
         [
-          InlineKeyboardButton("• ʜᴀɴᴅʟᴇʀ •", callback_data="dil_back")
+          InlineKeyboardButton("ʀɪᴠᴀʟs ᴋɪ ᴍᴋᴄ", callback_data="dil_back")
         ]]
     
     reply_markup = InlineKeyboardMarkup(buttons)
@@ -63,7 +63,7 @@ async def start(_, msg):
 gd_buttons = [              
         [
             InlineKeyboardButton("ᴏᴡɴᴇʀ", user_id=OWNER_ID),
-            InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/HEROKUFREECC"),    
+            InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/SHADOWS_Network01"),    
         ]
         ]
 
