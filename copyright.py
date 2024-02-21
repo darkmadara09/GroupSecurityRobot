fd460e9d32c2e9d1ffa0b0eac6e62f93e9f68e2e
darkmadara09 <136217534+darkmadara09@users.noreply.github.com>
2024-02-22 00:46:08 +0530

    Update copyright.py

--- copyright.py
+++ copyright.py
@@ -61,13 +61,11 @@ async def start(_, msg):
 
 
 gd_buttons = [              
-        [
             InlineKeyboardButton("ᴏᴡɴᴇʀ", user_id=OWNER_ID),
             InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/SHADOWS_Network01"),    
         ]
         [
             InlineKeyboardButton("ʀɪᴠᴀʟs ᴋɪ ᴍᴋᴄ", callback_data="dil_back")
-        ]
         ]
 
 
